9a725267129a5f745ccfb934b26ae175bf80320f
Atif-04 <atifsaif2004@gmail.com>
2023-11-11 05:16:33 +0000

    assignment-6

--- src/pages/EditStudent.js
+++ src/pages/EditStudent.js
@@ -1,13 +1,158 @@
 import React from 'react'
+import {Formik} from 'formik';
+import * as yup from 'yup';
+import { Routes,Route} from 'react-router-dom';
+import { Box ,TextField} from '@mui/material';
+import Button from '@mui/material/Button';
+import useMediaQuery from '@mui/material/useMediaQuery';
 
 function EditStudent() {
+  const handleFormSubmit =()=>{
+    console.log("Done")
+  }
+  const isNonMobile = useMediaQuery("(min-width:600px)");
   return (
     <>
     <h1>Edit Student</h1>
     <h4>Task: Create a Form here</h4>
-    
-</>
+    <Box  sx={{width:"50"}}>
+    <Formik
+    onSubmit={handleFormSubmit}
+    initialValues={initialValues}
+    validationSchema={checkoutSchema}
+    >
+      {({
+        values,
+        errors,
+        touched,
+        handleBlur,
+        handleChange,
+        handleSubmit,
+
+      })=>(
+        <form onSubmit={handleSubmit}>
+          <Box
+          display='grid'
+          
+          gap="30px"
+          gridTemplateColumns="repeat(4,minmax(0,1fr))"
+          sx={{
+            "& > div":{gridColumn:isNonMobile ? undefined:"span4"
+          },
+          }}
+          >
+         <TextField 
+         type="text"
+         label="Full-Name"
+         name="name"
+         onBlur={handleBlur}
+         onChange={handleChange}
+         error={!!touched.name&& !!errors.name}
+         helperText={touched.name&& errors.name}
+         sx={{padding:"5%"}}
+         />
+         
+          <TextField 
+         type="text"
+         label="Email id"
+         name="email"
+         onBlur={handleBlur}
+         onChange={handleChange}
+         error={!!touched.email&& !!errors.name}
+         helperText={touched.name&& errors.name}
+         sx={{padding:"5%"}}
+         />
+          <TextField 
+         type="text"
+         label="Email id"
+         name="email"
+         onBlur={handleBlur}
+         onChange={handleChange}
+         error={!!touched.email&& !!errors.name}
+         helperText={touched.name&& errors.name}
+         sx={{padding:"5%"}}
+         />
+         
+         <TextField 
+         type="text"
+         label="Full-Name"
+         name="name"
+         onBlur={handleBlur}
+         onChange={handleChange}
+         error={!!touched.name&& !!errors.name}
+         helperText={touched.name&& errors.name}
+         sx={{padding:"5%"}}
+         />
+
+         <TextField 
+         type="text"
+         label="Full-Name"
+         name="name"
+         onBlur={handleBlur}
+         onChange={handleChange}
+         error={!!touched.name&& !!errors.name}
+         helperText={touched.name&& errors.name}
+         sx={{padding:"5%"}}
+         />
+         
+         <TextField 
+         type="text"
+         label="Full-Name"
+         name="name"
+         onBlur={handleBlur}
+         onChange={handleChange}
+         error={!!touched.name&& !!errors.name}
+         helperText={touched.name&& errors.name}
+         sx={{padding:"5%"}}
+         />
+         <TextField 
+         type="text"
+         label="Full-Name"
+         name="name"
+         onBlur={handleBlur}
+         onChange={handleChange}
+         error={!!touched.name&& !!errors.name}
+         helperText={touched.name&& errors.name}
+         sx={{padding:"5%"}}
+         />
+         <TextField 
+         type="text"
+         label="Full-Name"
+         name="name"
+         onBlur={handleBlur}
+         onChange={handleChange}
+         error={!!touched.name&& !!errors.name}
+         helperText={touched.name&& errors.name}
+         sx={{padding:"5%"}}
+         />
+          </Box>
+          
+        </form>
+      )}
+    </Formik>
+    </Box>
+   <Routes>
+      <Route  exact path='editstudent'/>
+    </Routes>
+    <Button variant='contained'>Submit</Button>
+    </>
   )
 }
+const initialValues={
+  name:"",
+  email:"",
+  contact:"",
+
+}
+const checkoutSchema =yup.object().shape({
+  name: yup.string().required("required"),
+  email: yup.string().required("required"),
+  contact: yup.string().required("required"),
+  status: yup.string().required("required"),
+  
+})
+
+  
+
 
 export default EditStudent
--- src/pages/ViewStudents.js
+++ src/pages/ViewStudents.js
@@ -1,9 +1,76 @@
 import React from 'react'
+import { DataGrid } from '@mui/x-data-grid';
+import { Box,Stack} from '@mui/material';
+import { Link } from 'react-router-dom';
+import Button from '@mui/material/Button';
 
 function ViewStudents() {
+  const columns = [
+    { field: 'id', headerName: 'ID', width: 90 },
+    {
+      field: 'firstName',
+      headerName: 'First name',
+      width: 150,
+      editable: true,
+    },
+    {
+      field: 'lastName',
+      headerName: 'Last name',
+      width: 150,
+      editable: true,
+    },
+    {
+      field: 'age',
+      headerName: 'Age',
+      type: 'number',
+      width: 110,
+      editable: true,
+    },
+   
+  ];
+
+  const rows = [
+    { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
+    { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
+  ];
+    
+  
+
+    
+  
+ 
   return (
     <>
     <h1>DataGrid Here</h1>
+    <Box>
+      <Stack direction="row">
+        <Link to ='/dashboard/new'>
+          <Button variant='contained'>NewStudents</Button>
+        </Link>
+       
+        <Link to ='/dashboard/edit'>
+          <Button variant='contained'>EditStudents</Button>
+        </Link>
+      </Stack>
+    </Box>
+    <Box sx={{width: '100%' }}>
+      <DataGrid
+      autoHeight
+      
+        rows={rows}
+        columns={columns}
+        initialState={{
+          pagination: {
+            paginationModel: {
+              pageSize: 5,
+            },
+          },
+        }}
+        pageSizeOptions={[5]}
+        checkboxSelection
+        disableRowSelectionOnClick
+      />
+    </Box>
     </>
   )
 }
